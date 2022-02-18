b828af81ed406bb0275cccb6d00793e80ac3c77b
Alline <alline_franciely@hotmail.com>
2022-02-18 17:09:17 -0300

    lint

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import Missions from './components/Missions';
 class App extends React.Component {
   render() {
     return (
-      <div className='all'>
+      <div className="all">
         <Header />
         <SolarSystem />
         <Missions />
